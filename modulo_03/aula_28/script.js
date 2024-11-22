d24f33026bd7ca0d5a905f538dc1c6a5839e9e12
Bernardo Lopes <bernardolopesdev@gmail.com>
2024-11-22 17:10:31 -0300

    novos exercicios

---
+++ modulo_03/aula_28/script.js
@@ -0,0 +1,18 @@
+// Array
+let lista = ["Monitor", "Teclado", "Mouse"];
+console.log(lista.length);
+
+console.log(lista[0]);
+console.log(lista[1]);
+console.log(lista[2]);
+
+lista[0] = "WebCam";
+console.log(lista);
+
+lista[4] = "Monitor";
+console.log(lista);
+
+console.log(lista.length);
+
+// lista.length = lista.length - 2;
+// console.log(lista);
